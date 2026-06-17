7af2acd1eb2008c2af8b7eb5707560bf73afc5be
Yousef Qawwas <yusiqw@gmail.com>
2026-06-17 20:04:33 +0300

    home fragment

---
+++ app/src/main/java/com/example/a1221858_1220137_courseproject/HomeFragment.java
@@ -0,0 +1,27 @@
+package com.example.a1221858_1220137_courseproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.example.a1221858_1220137_courseproject.R;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        TextView tvWelcome = view.findViewById(R.id.tv_welcome_user);
+
+        return view;
+    }
+}
